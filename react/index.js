318bfb7343d2b75e9c942832c520f4ee48a49f39
Bruna Aleixo <bruna.aleixo97@hotmail.com>
2018-03-08 16:09:53 -0300

    add graphql queries

--- react/index.js
+++ react/index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
+import {graphql} from 'react-apollo'
 
+import homeDataQuery from './queries/home.graphql'
 import IntegrationsContainer from './containers/Integrations'
 import FeedContainer from './containers/Feed'
 
 // eslint-disable-next-line
-export default class Admin extends Component {
+class Admin extends Component {
   componentDidMount() {
     window.postMessage({ action: { type: 'STOP_LOADING' } }, '*')
   }
 
   render() {
+    const { data } = this.props
+
     return (
       <div className="ma4 flex flex-column flex-row-l">
         <IntegrationsContainer />
@@ -18,3 +22,7 @@ export default class Admin extends Component {
     )
   }
 }
+
+export default graphql(homeDataQuery, {
+  options: { ssr: false, variables: {productsTimePeriod: 0, pagePath: 'home', loadTimePeriod: 1} },
+})(Admin)
