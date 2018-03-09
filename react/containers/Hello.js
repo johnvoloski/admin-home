82a0828e0e88f2280fa21f8be85beed37199d005
Guilherme Rodrigues <gadr90@gmail.com>
2018-03-09 16:37:16 -0300

    Remove unrecognized dom props

--- react/containers/Hello.js
+++ react/containers/Hello.js
@@ -29,8 +29,8 @@ class Hello extends React.Component {
               <g className='nc-icon-wrapper' fill='none' transform='translate(.5 .5)'
                 stroke='#000' strokeWidth='3' strokeLinecap='round' strokeMiterlimit='10'
                 strokeLinejoin='round'>
-                <line dataColor='color-2' x1='54' y1='13' x2='10' y2='13' />
-                <rect dataColor='color-2' x='17' y='38' width='10' height='10' />
+                <line x1='54' y1='13' x2='10' y2='13' />
+                <rect x='17' y='38' width='10' height='10' />
                 <polyline points='8,36 8,61 36,61 36,43 48,43 48,61 56,61 56,36' />
                 <path d='M54,13V3H10v10L2,23 c0,3.866,3.134,7,7,7c2.234,0,4.218-1.05,5.5-2.679C15.782,28.95,17.766,30,20,30c2.551,0,4.777-1.369,6-3.408 C27.223,28.631,29.449,30,32,30s4.777-1.369,6-3.408C39.223,28.631,41.449,30,44,30c2.234,0,4.218-1.05,5.5-2.679 C50.782,28.95,52.766,30,55,30c3.866,0,7-3.134,7-7L54,13z'
                 />
